002c72e2b0cde5aa398dec1ca9518bd2d3ee90bb
Matias Gutierrez <matitobig@gmail.com>
2026-04-11 18:18:07 -0300

    ~1

--- dist/supabase/src/config.js
+++ dist/supabase/src/config.js
@@ -1,11 +1,13 @@
 import { expoEnv } from '../../constants/constants';
 import { createClient } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
+//import type AsyncStorageType from '@react-native-async-storage/async-storage'
 import 'react-native-url-polyfill/auto';
 import { Buffer } from 'buffer';
 global.Buffer = global.Buffer ?? Buffer;
 // AsyncStorage
 const isWeb = Platform.OS === 'web';
+//let asyncStorage: typeof AsyncStorageType | undefined = undefined
 let asyncStorage = undefined;
 if (!isWeb) {
     try {
--- src/supabase/src/config.ts
+++ src/supabase/src/config.ts
@@ -1,7 +1,7 @@
 import { expoEnv } from 'constants/constants'
 import { createClient } from '@supabase/supabase-js'
 import { Platform } from 'react-native'
-import type AsyncStorageType from '@react-native-async-storage/async-storage'
+//import type AsyncStorageType from '@react-native-async-storage/async-storage'
 
 import 'react-native-url-polyfill/auto'
 import { Buffer } from 'buffer'
@@ -12,7 +12,8 @@ global.Buffer = global.Buffer ?? Buffer
 // AsyncStorage
 const isWeb = Platform.OS === 'web'
 
-let asyncStorage: typeof AsyncStorageType | undefined = undefined
+//let asyncStorage: typeof AsyncStorageType | undefined = undefined
+let asyncStorage: any = undefined
 if (!isWeb) {
     try { asyncStorage = require('@react-native-async-storage/async-storage').default }
 	catch (error) { console.warn('AsyncStorage no disponible:', error) }
