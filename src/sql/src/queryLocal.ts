f54d80260540877f9969507bfdf2aa9c8a5d6916
Matias Gutierrez <matitobig@gmail.com>
2026-04-13 00:30:58 -0300

    ~1

--- src/sql/src/queryLocal.ts
+++ src/sql/src/queryLocal.ts
@@ -4,20 +4,20 @@ import dbRef from './init'
 
 const queryLocal = async ({
     query,
-    params,
-    getReturn=true
+    params=[],
+    onlyExecute=false
 
 }: {
     query: string
     params?: any[]
-    getReturn?: boolean
+    onlyExecute?: boolean
     
 }): Promise<any[]> => {
 
     if (!dbRef.current) throw new Error('Database not initialized')
 
     try{
-        if(getReturn) return await (dbRef.current.getAllAsync(query, params))
+        if(!onlyExecute) return await (dbRef.current.getAllAsync(query, params))
         await dbRef.current.execAsync(query)
     } catch (e) { console.error('Error queryLocal:', e, query.slice(0, 100), params) }
     return []
