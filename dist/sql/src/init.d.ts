418cea98dd6bf23d18728869f2a2641b0667c34b
Matias Gutierrez <matitobig@gmail.com>
2026-04-13 02:09:16 -0300

    ~1

--- dist/sql/src/init.d.ts
+++ dist/sql/src/init.d.ts
@@ -3,9 +3,22 @@ declare const dbRef: {
     current: SQLite.SQLiteDatabase | undefined;
 };
 export declare const init: ({ currentVersion, listQueriesTables, queryOnCreateTables, queryOnExists }: {
+    /**
+        Versión actual de la base de datos.
+        Se utiliza para reiniciarla a mano.
+    */
     currentVersion: number;
+    /**
+        Listado de queries para crear las tablas.
+    */
     listQueriesTables: string[];
+    /**
+        Cuando se crean las tablas, se ejecuta esta query.
+    */
     queryOnCreateTables?: string;
+    /**
+        Si la base de datos ya existía (no la está creando), se ejecuta esta query.
+    */
     queryOnExists?: string;
 }) => Promise<boolean>;
 export default dbRef;
--- src/sql/src/init.ts
+++ src/sql/src/init.ts
@@ -17,9 +17,22 @@ export const init = async ({
     queryOnExists
 
 }: {
+    /**
+        Versión actual de la base de datos.
+        Se utiliza para reiniciarla a mano.
+    */
     currentVersion: number
+    /**
+        Listado de queries para crear las tablas.
+    */
     listQueriesTables: string[]
+    /**
+        Cuando se crean las tablas, se ejecuta esta query.
+    */
     queryOnCreateTables?: string
+    /**
+        Si la base de datos ya existía (no la está creando), se ejecuta esta query.
+    */
     queryOnExists?: string
 
 }): Promise<boolean> => {
