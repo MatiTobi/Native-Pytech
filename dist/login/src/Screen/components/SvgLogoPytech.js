14fee26919e87c96f5f5cdd9361c20eed5e7b7d2
Matias Gutierrez <matitobig@gmail.com>
2026-04-13 17:26:10 -0300

    ~1

--- dist/login/src/Screen/components/SvgLogoPytech.js
+++ dist/login/src/Screen/components/SvgLogoPytech.js
@@ -1,6 +1,6 @@
 import { memo } from "react";
-import LogoSvg from '../../../../assets/images/logo_letras.svg';
-import LogoSvgDark from '../../../../assets/images/logo_letras_dark.svg';
+import LogoSvg from 'assets/images/logo_letras.svg';
+import LogoSvgDark from 'assets/images/logo_letras_dark.svg';
 import { useApp } from "../../../../providers/app";
 export default memo(({ size = 200 }) => {
     const { colorScheme } = useApp();
--- dist/tabs/src/components/Screen/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type Props from './types';
-declare const _default: ({ name, title, badge, iconNameIos, iconNameAndroid }: Props) => import("react").JSX.Element;
-export default _default;
