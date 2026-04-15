d37ff8d9f989c881f08de2cb123bf8373a6cc387
Matias Gutierrez <matitobig@gmail.com>
2026-04-15 10:27:13 -0300

    ~1

--- src/libs/tabs/src/components/Tabs/index.tsx
+++ src/libs/tabs/src/components/Tabs/index.tsx
@@ -1,4 +1,4 @@
-/*import { useSegments, Tabs } from 'expo-router';
+import { useSegments, Tabs } from 'expo-router';
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import React, { memo } from "react";
 
@@ -40,47 +40,5 @@ export default memo(({
 				/>
 			))}
 		</Tabs>
-	)
-})*/
-
-import { Badge, Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
-import { Tabs, useSegments } from 'expo-router';
-import React, { memo } from "react";
-
-import { capitalize } from 'libs/constants/utils';
-import Props from './types';
-
-
-
-export default memo(({
-	hidden=false,
-	onSegmentChange,
-	listTabs
-
-}: Props) => {
-
-	const segments = useSegments()
-	const hideTabBar = onSegmentChange?.({ segments }) ?? false
-
-	// tabBar Hidden
-	if (hidden) return (
-		<Tabs tabBar={() => null} screenOptions={{headerShown: false}}>
-			{listTabs.map((tab) => (<Tabs.Screen name={tab.name} />))}
-		</Tabs>
-	)
-  
-	return (
-		<NativeTabs
-			minimizeBehavior="onScrollDown"
-			blurEffect="dark" // Sólo se puede ver en iOS build app
-		>
-			{listTabs.map((tab) => (
-				<NativeTabs.Trigger key={tab.name} name={tab.name} role={tab.role}>
-					<Label>{tab.title || capitalize(tab.name)}</Label>
-					{tab.iconNameIos && <Icon sf={tab.iconNameIos} drawable="bottom_bar" />}
-					{tab.badge && <Badge>{String(tab.badge)}</Badge>}
-				</NativeTabs.Trigger>
-			))}
-		</NativeTabs>
 	)
 })
