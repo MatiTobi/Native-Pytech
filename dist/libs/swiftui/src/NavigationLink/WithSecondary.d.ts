42bb0d7b4d5f9efd77d7c88d506a2345f31da296
Matias Gutierrez <matitobig@gmail.com>
2026-04-28 11:57:36 -0300

    ~1

--- dist/libs/swiftui/src/NavigationLink/WithSecondary.d.ts
+++ dist/libs/swiftui/src/NavigationLink/WithSecondary.d.ts
@@ -4,8 +4,18 @@ import { Props as NavigationLinkProps } from './Wrapper';
 type Props = Omit<NavigationLinkProps, 'children'> & {
     /**
         Children to display on the left.
+        If not provided, the title will be displayed.
     */
     children?: React.ReactNode;
+    /**
+        Text to display on the left.
+        Will render if the children are not provided.
+    */
+    title?: string;
+    /**
+        Props to apply to the title.
+    */
+    titleTextProps?: TextProps;
     /**
         Secondary text to display on the right.
     */
@@ -15,5 +25,5 @@ type Props = Omit<NavigationLinkProps, 'children'> & {
     */
     secondaryTextProps?: TextProps;
 };
-declare const _default: React.MemoExoticComponent<({ children, secondaryText, secondaryTextProps, ...navigationLinkProps }: Props) => React.JSX.Element>;
+declare const _default: React.MemoExoticComponent<({ children, title, titleTextProps, secondaryText, secondaryTextProps, ...navigationLinkProps }: Props) => React.JSX.Element>;
 export default _default;
--- dist/libs/swiftui/src/NavigationLink/WithSecondary.js
+++ dist/libs/swiftui/src/NavigationLink/WithSecondary.js
@@ -2,10 +2,10 @@ import { HStack, Spacer } from '@expo/ui/swift-ui';
 import React, { memo } from 'react';
 import Text from '../Text';
 import NavigationLink from './Wrapper';
-export default memo(({ children, secondaryText, secondaryTextProps, ...navigationLinkProps }) => {
+export default memo(({ children, title, titleTextProps, secondaryText, secondaryTextProps, ...navigationLinkProps }) => {
     return (<NavigationLink {...navigationLinkProps}>
             <HStack>
-                {children}
+                {children || <Text {...titleTextProps}>{title}</Text>}
                 <Spacer />
                 <Text {...secondaryTextProps} secondary>{secondaryText}</Text>
             </HStack>
