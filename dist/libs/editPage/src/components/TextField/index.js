5fea06740c1ac913ef7036f9ca3f4a1bd2ffca46
Matias Gutierrez <matitobig@gmail.com>
2026-05-08 12:53:29 -0300

    ~1

--- dist/libs/editPage/src/components/TextField/index.js
+++ dist/libs/editPage/src/components/TextField/index.js
@@ -17,7 +17,7 @@ export default memo(({ defaultValue, placeholder, keyboardType, autocapitalizati
         const _value = value.trim() === '' ? null : value.trim();
         store.values[index].set({
             value: _value,
-            hasChanged: (_value !== defaultValue && defaultValue !== undefined),
+            hasChanged: _value !== defaultValue,
             isValid: isValid?.(_value) ?? true,
         });
     }, []);
