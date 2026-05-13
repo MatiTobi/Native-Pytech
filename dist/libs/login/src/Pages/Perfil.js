ba570724f4d4c2a7c1aaa0cbfc1d45ff17369add
Matias Gutierrez <matitobig@gmail.com>
2026-05-13 16:16:51 -0300

    ~1

--- dist/libs/login/src/Pages/Perfil.js
+++ dist/libs/login/src/Pages/Perfil.js
@@ -4,7 +4,7 @@ import { memo } from 'react';
 import Screen from '../Screen';
 import { handleSubmitLogInPerfil } from './utils';
 export default memo(({ routeOnSuccess, onSuccess }) => {
-    const { mail, first_name, gradient_text, color } = useLocalSearchParams();
+    const { mail, first_name, abbreviation, color } = useLocalSearchParams();
     const router = useRouter();
     const handleSubmit = async ({ value }) => {
         const { succeded, message } = await handleSubmitLogInPerfil({ mail, password: value });
@@ -15,5 +15,5 @@ export default memo(({ routeOnSuccess, onSuccess }) => {
         }
         return { succeded, message };
     };
-    return (<Screen iconPage={<Screen.Gradient text={`${gradient_text}`} color={color}/>} title={`¡Hola${first_name ? ` ${first_name}` : ''}!`} bottomElements={<Screen.Input placeholder='Contraseña' keyboardType='default' secureTextEntry={true} autoComplete='password' handleSubmit={handleSubmit} autoFocus={Platform.OS === 'web'}/>}/>);
+    return (<Screen iconPage={<Screen.Gradient text={`${abbreviation}`} color={color}/>} title={`¡Hola${first_name ? ` ${first_name}` : ''}!`} bottomElements={<Screen.Input placeholder='Contraseña' keyboardType='default' secureTextEntry={true} autoComplete='password' handleSubmit={handleSubmit} autoFocus={Platform.OS === 'web'}/>}/>);
 });
