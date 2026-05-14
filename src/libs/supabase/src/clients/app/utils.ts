a5826d319b2beccd8e9c749801761fc169e53e79
Matias Gutierrez <matitobig@gmail.com>
2026-05-14 17:37:27 -0300

    ~1

--- src/libs/supabase/src/clients/app/utils.ts
+++ src/libs/supabase/src/clients/app/utils.ts
@@ -27,7 +27,3 @@ export const execFunction = async (params: utils.types.ExecFunctionParams) => (
     await utils.execFunction(supabase, params)
 )
 
-
-export const signUp = async (params: utils.types.SignUpParams) => (
-    await utils.signUp(supabase, params)
-)
