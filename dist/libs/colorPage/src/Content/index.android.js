a68afa265ef1c1efbfe5b955b6a419c39c5eb29b
Matias Tobias Gutierrez <matitobig@gmail.com>
2026-05-18 18:37:54 -0300

    .

--- dist/libs/colorPage/src/Content/index.android.js
+++ dist/libs/colorPage/src/Content/index.android.js
@@ -1,10 +1,12 @@
 import { Host, Column, Row } from '@expo/ui/jetpack-compose';
-import React, { memo } from 'react';
+import React, { Fragment, memo } from 'react';
 export default memo(({ colorRows, renderItem, ...pageProps }) => {
     return (<Host style={{ flex: 1 }}>
 			<Column verticalArrangement={{ spacedBy: 16 }}>
 				{colorRows.map((row, index) => (<Row key={index} verticalArrangement="spaceEvenly">
-						{row.map((color, indexColor) => (renderItem?.({ color, size: 55, ...pageProps })))}
+						{row.map((color, indexColor) => renderItem == null ? null : (<Fragment key={indexColor}>
+								{React.createElement(renderItem, { color, size: 55, ...pageProps })}
+							</Fragment>))}
 					</Row>))}
 			</Column>
 		</Host>);
