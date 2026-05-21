258ce164b6301b8f59b8c8c3257d908dc47a5bcd
Matias Gutierrez <matitobig@gmail.com>
2026-05-21 09:44:53 -0300

    ~1

--- dist/libs/swiftui/src/components/NavigationLink/index.d.ts
+++ dist/libs/swiftui/src/components/NavigationLink/index.d.ts
@@ -1,4 +1,4 @@
 import React from 'react';
 import type Props from './types';
-declare const _default: React.MemoExoticComponent<({ children, onPress, icon, label, systemImage, listRowInsets, textTrailing, textTrailingProps }: Props) => React.JSX.Element>;
+declare const _default: React.MemoExoticComponent<({ children, onPress, icon, label, systemImage, listRowInsets, trailingText, trailingTextProps }: Props) => React.JSX.Element>;
 export default _default;
