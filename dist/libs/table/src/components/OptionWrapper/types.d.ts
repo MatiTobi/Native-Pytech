fc9765f4c30864684d8b0907ab638dee035d16ea
Matias Gutierrez <matitobig@gmail.com>
2026-05-26 11:13:20 -0300

    ~1

--- dist/libs/table/src/components/OptionWrapper/types.d.ts
+++ dist/libs/table/src/components/OptionWrapper/types.d.ts
@@ -5,10 +5,12 @@ import OptionProps from "../Option/types";
 import { DeleteIcon, DragIcon, Icon, Image, Text, TextInput, TextInputCurrency, TextView } from '../OptionComponents';
 export type Props = OptionProps & {
     id: string;
-    isAnimated: boolean;
     /**
-        Borde de la opción.
-        Es un dict con { left: 17, right: 16 } por defecto.
+        @default false
+    */
+    isAnimated?: boolean;
+    /**
+        @default '{ left: left, right: right, shownTop: null, shownBottom: null, color: null }'
     */
     borders?: {
         color: string | null;
