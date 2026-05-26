f1d8a841e780fc6100ac1c20b265d24ed95ede1a
Matias Gutierrez <matitobig@gmail.com>
2026-05-26 13:56:24 -0300

    ~1

--- dist/libs/table/src/components/Table/types.d.ts
+++ dist/libs/table/src/components/Table/types.d.ts
@@ -8,7 +8,7 @@ type Props = {
         Opciones de la tabla.
         Tiene que ser una lista de Options.
     */
-    children?: React.ReactElement[];
+    children?: React.ReactElement[] | React.ReactElement;
     /**
         Texto que aparece arriba de la tabla.
         Si es null, no se muestra.
