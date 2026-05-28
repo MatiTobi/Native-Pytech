b39501e5bca7fce401a1e11de05ad8f2801894dd
Matias Gutierrez <matitobig@gmail.com>
2026-05-28 11:44:02 -0300

    ~1

---
+++ dist/libs/sections/Icon/src/index.d.ts
@@ -0,0 +1,3 @@
+import Props from '../types';
+declare const _default: import("react").MemoExoticComponent<({ title, subtitle, gradientProps, buttonProps }: Props) => import("react").JSX.Element>;
+export default _default;
---
+++ dist/libs/sections/Icon/src/index.ios.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import Props from '../types';
+declare const _default: React.MemoExoticComponent<({ title, subtitle, gradientProps, buttonProps }: Props) => React.JSX.Element>;
+export default _default;
