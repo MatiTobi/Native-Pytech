cd6b6f208a7f64bc5c1f1fe29e7175e1a4e95070
Matias Gutierrez <matitobig@gmail.com>
2026-05-28 19:55:01 -0300

    ~1

--- src/libs/colorPage/src/Content/index.tsx
+++ src/libs/colorPage/src/Content/index.tsx
@@ -1,2 +1,36 @@
-import type Props from "./types";
-export default ({...props}: Props) => null
+import { memo } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import Item from '../Item';
+import type Props from './types';
+
+
+
+export default memo(({
+	colorRows,
+	...pageProps
+
+}: Props) => {
+	return (
+		<View style={styles.container}>
+			{colorRows.map((row, index) => (
+				<View key={index} style={styles.row}>
+					{row.map((color, indexColor) => (
+						<Item key={indexColor} color={color} size={100} {...pageProps}/>
+					))}
+				</View>
+			))}
+		</View>
+	)
+})
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'space-evenly',
+	},
+	row: {
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+	},
+})
--- src/libs/colorPage/src/Item/index.tsx
+++ src/libs/colorPage/src/Item/index.tsx
@@ -1,2 +1,46 @@
-import type Props from "./types";
-export default ({...props}: Props) => null
+import { memo } from 'react';
+import { colors } from 'native-pytech/components/gradient';
+import { Pressable, StyleSheet } from 'react-native';
+
+import Gradient from 'native-pytech/components/gradient';
+
+import type Props from './types';
+
+
+
+export default memo(({
+	color,
+	size,
+	selectedColor,
+	onSelectColor,
+
+}: Props) => {
+
+	const isSelected = color === selectedColor
+
+	return (
+		<Pressable
+			onPress={() => onSelectColor?.(color)}
+			style={[
+				styles.container,
+				{
+					borderColor: isSelected ? colors[color].light : 'transparent',
+				}
+			]}
+		>
+			<Gradient
+				color={color}
+				type='extraLarge'
+			/>
+		</Pressable>
+	)
+})
+
+
+const styles = StyleSheet.create({
+	container: {
+		borderWidth: 4,
+		borderRadius: '100%',
+		padding: 4,
+	},
+})
