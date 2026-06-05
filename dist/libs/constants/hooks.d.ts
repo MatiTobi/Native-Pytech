1ebc76fe78aeca920a896744238e2f5adcaa9d47
Matias Gutierrez <matitobig@gmail.com>
2026-06-05 17:30:56 -0300

    ~1

--- dist/libs/constants/hooks.d.ts
+++ dist/libs/constants/hooks.d.ts
@@ -1,8 +1,8 @@
 declare const Hooks: {
     useEffectWithoutFirstRender: (effect: () => void, deps: any[]) => void;
     useLayoutEffectWithoutFirstRender: (effect: () => void, deps: any[]) => void;
-    useAsyncEffect: (effect: (isMounted: () => boolean) => Promise<void>, deps: any[]) => void;
-    useAsyncFocusEffect: (effect: (isMounted: () => boolean) => Promise<void>) => void;
-    useAsyncFocusEffectWithoutFirstRender: (effect: (isMounted: () => boolean) => Promise<void>) => void;
+    useAsyncEffect: (effect: (isMounted: () => boolean) => Promise<any>, deps: any[]) => void;
+    useAsyncFocusEffect: (effect: (isMounted: () => boolean) => Promise<any>) => void;
+    useAsyncFocusEffectWithoutFirstRender: (effect: (isMounted: () => boolean) => Promise<any>) => void;
 };
 export default Hooks;
