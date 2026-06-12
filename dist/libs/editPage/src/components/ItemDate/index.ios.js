1bb47279d26d0b9ab24715b856747056a9cdb92e
Matias Gutierrez <matitobig@gmail.com>
2026-06-12 15:50:31 -0300

    ~1

--- dist/libs/editPage/src/components/ItemDate/index.ios.js
+++ dist/libs/editPage/src/components/ItemDate/index.ios.js
@@ -1,18 +1,31 @@
 import { DatePicker } from '@expo/ui/swift-ui';
-import { memo, useCallback, useEffect, useRef, useState } from 'react';
+import { memo, useCallback, useEffect, useRef, useState, useMemo } from 'react';
 import { environment } from '@expo/ui/swift-ui/modifiers';
 import { usePage } from '../../context/page';
+import Hooks from '../../../../../libs/constants/hooks';
 export default memo(({ itemKey, label, selection, defaultValue, minDate, maxDate, onValueChange, }) => {
     const { store, registerItem } = usePage();
     const [_selection, setSelection] = useState(selection);
+    const currentDate = useMemo(() => _selection ?? defaultValue ?? new Date(), [_selection, defaultValue]);
+    const currentDateRanged = useMemo(() => {
+        if (maxDate && currentDate > maxDate)
+            return maxDate;
+        if (minDate && currentDate < minDate)
+            return minDate;
+        return currentDate;
+    }, [currentDate, minDate, maxDate]);
     // Key
     const keyRef = useRef(itemKey);
     useEffect(() => {
         keyRef.current = registerItem(itemKey);
-        _onValueChange(_selection ?? defaultValue ?? new Date());
+        _onValueChange(currentDate);
     }, []);
     // Hooks
     useEffect(() => setSelection(selection), [selection]);
+    Hooks.useEffectWithoutFirstRender(() => {
+        setSelection(currentDateRanged);
+        _onValueChange(currentDateRanged);
+    }, [currentDateRanged]);
     const _onValueChange = useCallback((value) => {
         setSelection(value);
         store.values[keyRef.current ?? 0].set({
@@ -22,7 +35,7 @@ export default memo(({ itemKey, label, selection, defaultValue, minDate, maxDate
         });
         onValueChange?.(value);
     }, []);
-    return (<DatePicker title={label} selection={_selection ?? defaultValue} onDateChange={_onValueChange} modifiers={[environment('locale', 'es_ES')]} range={{
+    return (<DatePicker title={label} selection={currentDate} onDateChange={_onValueChange} modifiers={[environment('locale', 'es_ES')]} range={{
             start: minDate,
             end: maxDate,
         }}/>);
