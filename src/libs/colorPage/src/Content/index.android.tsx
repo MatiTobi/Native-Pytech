5c99b371a1f0292f9c6e6562cba83a2053746c7b
Matias Tobias Gutierrez <matitobig@gmail.com>
2026-06-29 15:40:57 -0300

    .

--- src/libs/colorPage/src/Content/index.android.tsx
+++ src/libs/colorPage/src/Content/index.android.tsx
@@ -14,17 +14,15 @@ export default memo(({
 
 }: Props) => {
 	return (
-		<Host matchContents={{ vertical: true }} style={{ width: '100%' }}>
-			<Column verticalArrangement={{ spacedBy: 2 }}>
-				{colorRows.map((row, index) => (
-					<Row key={index} horizontalArrangement="spaceEvenly" verticalArrangement="center" modifiers={[ fillMaxWidth() ]}>
-						{row.map((color, indexColor) => (
-							<Item key={color} color={color} size={55} {...pageProps} />
-						))
-						}
-					</Row>
-				))}
-			</Column>
-		</Host>
+		<Column verticalArrangement={{ spacedBy: 2 }}>
+			{colorRows.map((row, index) => (
+				<Row key={index} horizontalArrangement="spaceEvenly" verticalArrangement="center" modifiers={[ fillMaxWidth() ]}>
+					{row.map((color, indexColor) => (
+						<Item key={color} color={color} size={55} {...pageProps} />
+					))
+					}
+				</Row>
+			))}
+		</Column>
 	)
 })
